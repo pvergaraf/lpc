42f31d6814ae3e5d87d925cc5d636ac4eb17f637
Pablo Vergara <pablo.vergara@gmail.com>
2025-03-10 20:54:05 -0300

    settings edit

--- club_project/settings.py
+++ club_project/settings.py
@@ -159,14 +159,15 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
-# Use S3 in production
+# Storage settings
 if not DEBUG:
     # AWS S3 Settings
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     
     # Static files configuration
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+    AWS_LOCATION = 'static'
     
     # Media files configuration
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
