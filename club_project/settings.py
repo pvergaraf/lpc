3376f1fdca567f6522983ac0556d47b106af82e3
Pablo Vergara <pablo.vergara@gmail.com>
2025-03-10 19:04:36 -0300

    settings.py edited

--- club_project/settings.py
+++ club_project/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o=r9k=d-sxe61+rsya_)ryaogt7ut4v&^qjh+vny59n6a@x5rb'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-o=r9k=d-sxe61+rsya_)ryaogt7ut4v&^qjh+vny59n6a@x5rb')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'club_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
