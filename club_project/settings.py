6ff0ed40066cca705fea7a41f2fc77f3a996d083
Pablo Vergara <pablo.vergara@gmail.com>
2025-03-10 23:00:46 -0300

    settings

--- club_project/settings.py
+++ club_project/settings.py
@@ -198,13 +198,19 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 STORAGES = {
     "default": {
         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "OPTIONS": {
+            "location": "media"
+        },
     },
     "staticfiles": {
         "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+        "OPTIONS": {
+            "location": "static"
+        },
     },
 }
 
-# URLs and paths
+# URLs for static and media files
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
